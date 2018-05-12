70e9da2a827faa90da70372f0e00615d38853312
Nikita <captflint3561@gmail.com>
2018-05-12 23:59:03 +0300

    update to task7 v2.0

--- task7.cpp
+++ task7.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <fstream>
 #include <vector>
 #include <iterator>
@@ -11,58 +12,110 @@ ofstream file1("first.txt", ios::app);
 class Books {
 public:
     virtual void print() = 0;
-    virtual void get() = 0;
 };
 
 class Name {
 public:
-    void name(char mas[SIZE]) {
-        file1 << "Название книги: " << mas << "\t";
+    void name (char mas[SIZE]) {
+        file1 << "Volume Name: " << mas << "\t";
+    }
+    void name1 () {
+        cout << "Класс имя"<< endl;
     }
 };
 
 class Type {
 public:
     void type (char mas1[SIZE]) {
-        file1 << "Тип книги: " << mas1 << "\t";
+        file1 << "Book type: " << mas1 << "\t";
+    }
+    void type1 () {
+        cout << "Класс тип"<< endl;
     }
 };
 class Print {
 public:
     void print (char mas2[SIZE]) {
-        file1 << "Частота выпуска: " << mas2 << "\t";
+        file1 << "Release frequency: " << mas2 << "\t";
+    }
+    void print1 () {
+        cout << "Класс частота выпуска"<< endl;
     }
 };
 
 class Pages {
 public:
     void pages (int mas3[SIZE]) {
-        file1 << "Кол-во страниц: " << *mas3 << "\t";
+        file1 << "Number of pages: " << *mas3 << "\t";
+    }
+    void pages1 () {
+        cout << "Класс кол-во страниц"<< endl;
     }
 };
 
 class Edition {
 public:
     void edition (int mas4[SIZE]) {
-        file1 << "Тираж в шт.: " << *mas4 << "\t";
+        file1 << "Edition: " << *mas4 << "\t";
+    }
+    void edition1 () {
+        cout << "Класс тираж"<< endl;
     }
 };
 
-class Year{
+class Year {
 public:
     void year (int mas5[SIZE]) {
-        file1 << "Год издания: " << *mas5 << endl << endl;
+        file1 << "The year of publishing: " << *mas5 << ";" << endl << endl;
+    }
+    void year1() {
+        cout << "Класс год выпуска" << endl;
     }
 };
 
-class Run : public Year, public Edition, public Print, public Pages, public Type, public Name {
+class Run : public Name, public Year, public Edition, public Print, public Pages, public Type {
 public:
+    void run() {
+        int k = 0;
+        cout << "Вы хотите отчистить файл?" << endl;
+        cin >> k;
+        if (k == 1) {
+            ofstream file1;
+            file1.open("first.txt", ofstream::out | ofstream::trunc);
+            cout << "Файл был отчищен." << endl;
+            file1.close();
+        } else {
+            cout << "Файл не был отчищен." << endl;
+            file1.close();
+        }
+    }
 };
 
 class Search {
 public:
-    void print (char mas[SIZE])  {
+    void search (char mas[SIZE])  {
         ifstream file1("first.txt", ios::in);
+        vector<char> mas9;
+        vector<char> mas10(SIZE);
+        
+        istream_iterator<char> begin(file1);
+        istream_iterator<char> end;
+        char mas2 = mas[SIZE];
+        copy (begin, end, back_inserter(mas9));
+        
+        if  (find(mas9.begin(), mas9.end(), mas2) != mas9.end()) {
+            cout << "Такая книга есть в списке" << endl;
+        } else {
+            cout << "данных нет" << endl;
+        }
+    }
+};
+
+class Prints: public Books {
+public:
+    void print ()  {
+        ifstream file1("first.txt", ios::in);
+        
         vector<char> mas9;
         vector<char> mas10(SIZE);
         
@@ -71,18 +124,34 @@ public:
         
         copy (begin, end, back_inserter(mas9));
         
-        // copy(mas9.begin(), mas9.end(), ostream_iterator<char>(cout));
         
-        if  (find(mas9.begin(), mas9.end(), mas[SIZE]) != mas9.end()){
-            cout << "данные найдены" << endl;
-        } else {
-            cout << "данных нет" << endl;
+        for (int i = 1; i < mas9.size(); i++) {
+            if (mas9[i] == ';') {
+                mas9[i] = 0x0A;
+            }
+        }
+        copy(mas9.begin(), mas9.end(), ostream_iterator<char>(cout, ""));
+        file1.close();
+    }
+};
+
+class Error {
+public:
+    void Errors () {
+        try {
+            if (!file1) {
+                int a = 1;
+            }
+        } catch (int a) {
+            cout << "Ошибка при открытии файла" << endl;
         }
     }
 };
 
 int main(int argc, const char * argv[]) {
     setlocale(0,"");
+    Error er;
+    er.Errors();
     int z = 1;
     for(;;) {
         if (z == 1) {
@@ -139,10 +208,32 @@ int main(int argc, const char * argv[]) {
             break;
         }
     }
+    
     Search k;
     char *mas10 = new char [SIZE];
     gets(mas10);
-    k.print(mas10);
+    k.search(mas10);
+    
+    Prints p;
+    p.print();
+    
+    Run r;
+    r.run();
+    
+    r.name1();
+    r.type1();
+    r.print1();
+    r.pages1();
+    r.edition1();
+    r.year1();
     return 0;
 }
 
+/*
+ 
+ Volume[__] = { 1, 2, 3, 4, 5}
+ Volume[i] = 4
+ 
+ Volume[__] = { 1, 2, 3, \0 ,5}
+ 
+ */
